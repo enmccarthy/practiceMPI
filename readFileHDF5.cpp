3d8cac66a8127ae5882318d52fc03b77bf3640b4
Erin Mccarthy <emccarth@lassen708.coral.llnl.gov>
2019-10-07 16:27:22 -0700

    fixed indentation errors from changing vim settings

--- readFileHDF5.cpp
+++ readFileHDF5.cpp
@@ -79,113 +79,113 @@ int main(int argc, char *argv[])
    H5Pset_fapl_mpio(fapl_id, m_comm, mpi_info);
    short int data_out[512*512*(512/4)*4];
    double start = MPI_Wtime();
-   for(int nux = 0; nux<numsamples; nux+= nprocs/4) {
-      hid_t file, dataset, filespace, memspace;
-      herr_t status, status_n;  
-      filename = files[nux];
-      file = H5Fopen(filename.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
-      hsize_t num_obj;
-      char name[100];
+  for(int nux = 0; nux<numsamples; nux+= nprocs/4) {
+    hid_t file, dataset, filespace, memspace;
+    herr_t status, status_n;  
+    filename = files[nux];
+    file = H5Fopen(filename.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
+    hsize_t num_obj;
+    char name[100];
     
-      H5Gget_num_objs(file, &num_obj);
-      H5Gget_objname_by_idx(file, 0, name, 100);
+    H5Gget_num_objs(file, &num_obj);
+    H5Gget_objname_by_idx(file, 0, name, 100);
           
-      dataset = H5Dopen(file, name, H5P_DEFAULT);
-      int rank_data;
-      filespace = H5Dget_space(dataset);
-      // can I use this to get the rank???/
-      rank_data = H5Sget_simple_extent_ndims(filespace);
+    dataset = H5Dopen(file, name, H5P_DEFAULT);
+    int rank_data;
+    filespace = H5Dget_space(dataset);
+    // can I use this to get the rank???/
+    rank_data = H5Sget_simple_extent_ndims(filespace);
 
-      // get total buf size
-      hsize_t dims[RANK];
-      hsize_t count[RANK];
-      hsize_t offset[RANK];
-      // unsure if I need this
-      int rank_chunk;
-      hsize_t i, j;
+    // get total buf size
+    hsize_t dims[RANK];
+    hsize_t count[RANK];
+    hsize_t offset[RANK];
+    // unsure if I need this
+    int rank_chunk;
+    hsize_t i, j;
     
-      status_n = H5Sget_simple_extent_dims(filespace, dims, NULL);
-      int x = dims[0];
-      int y = dims[1];
-      int z = dims[2];
-      int s = dims[3]; 
+    status_n = H5Sget_simple_extent_dims(filespace, dims, NULL);
+    int x = dims[0];
+    int y = dims[1];
+    int z = dims[2];
+    int s = dims[3]; 
     
-      int ylines = 1;
-      int xlines = 1;
-      int zlines = 4;
-      int samplelines = 1;
+    int ylines = 1;
+    int xlines = 1;
+    int zlines = 4;
+    int samplelines = 1;
         
-      int xPerNode = x/xlines;
-      int yPerNode = y/ylines;
-      int zPerNode = z/zlines;
-      int sPerNode = s/samplelines;
+    int xPerNode = x/xlines;
+    int yPerNode = y/ylines;
+    int zPerNode = z/zlines;
+    int sPerNode = s/samplelines;
         
-      //short int data_out[yPerNode*xPerNode*zPerNode*sPerNode];  
-      hsize_t dims_local[4];
-      dims_local[3] = sPerNode;
-      dims_local[2] = zPerNode;
-      dims_local[1] = yPerNode;
-      dims_local[0] = xPerNode;
-      memspace = H5Screate_simple(RANK, dims_local, NULL);
-      int num_io_parts = 4;
-      int odd_offset;
-      if (xlines > 1) {
-        // this is theoretically the odd case but it has not been tested yet
-        // so everything with odd should be ignored
-        if((rank%num_io_parts) < int(dims[0]%xPerNode)) {
-          offset[0] = ((xPerNode+1)*(rank%num_io_parts));
-        } else {
-          // offset of this x dim for this rank;
-          // in most cases odd_offset will be 0
-          odd_offset = (xPerNode+1)*(dims[0]%xPerNode);
-          offset[0] = odd_offset +(xPerNode*((rank%num_io_parts)%xlines));
-        }
+    //short int data_out[yPerNode*xPerNode*zPerNode*sPerNode];  
+    hsize_t dims_local[4];
+    dims_local[3] = sPerNode;
+    dims_local[2] = zPerNode;
+    dims_local[1] = yPerNode;
+    dims_local[0] = xPerNode;
+    memspace = H5Screate_simple(RANK, dims_local, NULL);
+    int num_io_parts = 4;
+    int odd_offset;
+    if (xlines > 1) {
+      // this is theoretically the odd case but it has not been tested yet
+      // so everything with odd should be ignored
+      if((rank%num_io_parts) < int(dims[0]%xPerNode)) {
+        offset[0] = ((xPerNode+1)*(rank%num_io_parts));
       } else {
-        offset[0] = 0;
+        // offset of this x dim for this rank;
+        // in most cases odd_offset will be 0
+        odd_offset = (xPerNode+1)*(dims[0]%xPerNode);
+        offset[0] = odd_offset +(xPerNode*((rank%num_io_parts)%xlines));
       }
+    } else {
+      offset[0] = 0;
+    }
 
-      if (ylines > 1) {
-        if((rank%num_io_parts) < int(dims[1]%yPerNode)) {
-          offset[1] = ((yPerNode+1)*(rank%num_io_parts));
-        } else {
-          // offset of the y dim for this rank
-          odd_offset = (yPerNode+1)*(dims[1]%yPerNode);
-          offset[1] = odd_offset + (yPerNode*((rank%num_io_parts)/ylines));
-        }
+    if (ylines > 1) {
+      if((rank%num_io_parts) < int(dims[1]%yPerNode)) {
+        offset[1] = ((yPerNode+1)*(rank%num_io_parts));
       } else {
-        offset[1] = 0;
+        // offset of the y dim for this rank
+        odd_offset = (yPerNode+1)*(dims[1]%yPerNode);
+        offset[1] = odd_offset + (yPerNode*((rank%num_io_parts)/ylines));
       }
+    } else {
+      offset[1] = 0;
+    }
 
-      if (zlines > 1) {
-        offset[2] = zPerNode*(rank%num_io_parts);
-      } else {
-        offset[2] = 0;
-      }
-      // I have channel dim splits 
-      // all combos arent really tested
-      //add the rest later
-      offset[3] = 0;
+    if (zlines > 1) {
+      offset[2] = zPerNode*(rank%num_io_parts);
+    } else {
+      offset[2] = 0;
+    }
+    // I have channel dim splits 
+    // all combos arent really tested
+    //add the rest later
+    offset[3] = 0;
  
-      count[0] = 1;
-      count[1] = 1;
-      count[2] = 1;
-      count[3] = 1;     
-      // start -> a starting location for the hyperslab
-      // stride -> the number of elements to separate each element or block to be selected
-      // count -> the number of elemenets or blocks to select along each dimension
-      // block -> the size of the block selected from the dataspace 
-      //I think stride can be null
-      //std::cout<< "offset " << offset[0] <<" "<< offset[1] << " "<< offset [2] << " " << offset[3] <<"\n";
-      //std::cout<< "block " << block[0] <<" "<< block[1] << " "<< block[2] << " " << block[3] <<"\n";
-      status = H5Sselect_hyperslab(filespace, H5S_SELECT_SET, offset, NULL, 
+    count[0] = 1;
+    count[1] = 1;
+    count[2] = 1;
+    count[3] = 1;     
+    // start -> a starting location for the hyperslab
+    // stride -> the number of elements to separate each element or block to be selected
+    // count -> the number of elemenets or blocks to select along each dimension
+    // block -> the size of the block selected from the dataspace 
+    //I think stride can be null
+    //std::cout<< "offset " << offset[0] <<" "<< offset[1] << " "<< offset [2] << " " << offset[3] <<"\n";
+    //std::cout<< "block " << block[0] <<" "<< block[1] << " "<< block[2] << " " << block[3] <<"\n";
+    status = H5Sselect_hyperslab(filespace, H5S_SELECT_SET, offset, NULL, 
                     count, dims_local);
-      status = H5Dread(dataset, H5T_NATIVE_SHORT, memspace, filespace, 
+    status = H5Dread(dataset, H5T_NATIVE_SHORT, memspace, filespace, 
                        H5P_DEFAULT, data_out);
       //std::cout<<data_out[10]<<"\n";
       //MPI_Comm_free(&file_com);
        
-      H5Dclose(dataset);
-      H5Fclose(file);
+    H5Dclose(dataset);
+    H5Fclose(file);
   }
   double end = MPI_Wtime();
     //MPI_Group_free(&world_group);
